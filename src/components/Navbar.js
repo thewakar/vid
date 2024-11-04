71b0af26cd52b34ba7482d37265c298606462bbc
thewakar <thewakar1@gmail.com>
2024-11-04 20:05:59 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Video Wall</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
