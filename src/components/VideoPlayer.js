eddaf792241ab1c6f5abf4d83a873bc505b31f2d
thewakar <thewakar1@gmail.com>
2024-11-05 10:30:04 +0530

    Update VideoPlayer.js

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 
 const VideoPlayer = () => {
-    const [videoId, setVideoId] = useState('dQw4w9WgXcQ'); // Example Video ID
+    const [videoId, setVideoId] = useState('u_nK7VAphY0sON1F'); // Example Video ID
 
     const changeVideo = (newVideoId) => {
         setVideoId(newVideoId);
@@ -12,15 +12,15 @@ const VideoPlayer = () => {
     return (
         <div className="text-center">
             <ReactPlayer
-                url={`https://www.youtube.com/watch?v=${videoId}`}
+                url={`https://www.youtu.be/9vyoezhwCYY?si=${videoId}`}
                 controls
                 width="640px"
                 height="360px"
             />
             <div className="mt-4">
-                <button className="btn btn-primary mx-2" onClick={() => changeVideo('dQw4w9WgXcQ')}>Video 1</button>
-                <button className="btn btn-primary mx-2" onClick={() => changeVideo('3JZ_D3ELwOQ')}>Video 2</button>
-                <button className="btn btn-primary mx-2" onClick={() => changeVideo('hY7m5jjJ9mI')}>Video 3</button>
+                <button className="btn btn-primary mx-2" onClick={() => changeVideo('u_nK7VAphY0sON1F')}>Video 1</button>
+                <button className="btn btn-primary mx-2" onClick={() => changeVideo('u_nK7VAphY0sON1F')}>Video 2</button>
+                <button className="btn btn-primary mx-2" onClick={() => changeVideo('u_nK7VAphY0sON1F')}>Video 3</button>
             </div>
         </div>
     );
